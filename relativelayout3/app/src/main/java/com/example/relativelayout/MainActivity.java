37e593bce1339cd54252280bcef62e284d822b5e
Aneeta L R <95289853+h-c-n@users.noreply.github.com>
2022-10-20 11:30:33 +0530

    relative

---
+++ relativelayout3/app/src/main/java/com/example/relativelayout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.relativelayout;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.relativelayout.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.b1).setOnClickListener(view -> {
+            Toast toast = new Toast(this);
+            toast.setView(LayoutInflater.from(this).inflate(R.layout.activity_toast,null));
+            toast.show();
+        });
+    }
+}
